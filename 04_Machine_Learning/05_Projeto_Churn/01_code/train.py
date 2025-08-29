e142ab5a10f9ed9db761f004115ea16894b85538
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-08-29 17:46:45 -0300

    projeto churn

--- 04_Machine_Learning/05_Projeto_Churn/01_code/train.py
+++ 04_Machine_Learning/05_Projeto_Churn/01_code/train.py
@@ -1,17 +1,17 @@
 # %%
+#============================ SAMPLE ============================
 import pandas as pd
 
 df = pd.read_csv("../data/abt_churn.csv")
 df.head()
-
 # %% Safras do out of time
-oot = df[df['dtRef'] == df['dtRef'].max()].copy
+oot = df[df['dtRef'] == df['dtRef'].max()].copy()
 
 # %%
 df_train = df[df['dtRef'] < df['dtRef'].max()].copy()
 
 # %% Separando variaveis e target
-features = df_train.columns[2:-1] # não pega a data, nem o target
+features = df_train.columns[2:-1] # não pega a data(dia), nem o target
 target = "flagChurn"
 
 X, y = df_train[features], df_train[target]
@@ -46,4 +46,175 @@ print("Taxa variável resposta Geral:", y.mean())
 print("Taxa variável resposta Treino:", y_train.mean())
 print("Taxa variável resposta: Teste", y_test.mean())
 
+
+# %% 
+#============================ EXPLORE ===========================
+X_train.isna().sum().sort_values(ascending=False) # verifica se ha dados faltantes
+
+# %%
+pd.set_option('display.max_rows', 80)
+pd.set_option('display.max_columns', None)
+df_analise = X_train.copy()
+df_analise[target] = y_train
+sumario = df_analise.groupby(by=target).agg(["mean", "median"]).T
+sumario
+
+# %%
+sumario['diff_abs'] = sumario[0] - sumario[1]
+sumario['diff_rel'] = sumario[0]/sumario[1]
+sumario.sort_values(by=['diff_rel'], ascending=False)
+
+# %%
+from sklearn import tree
+import matplotlib.pyplot as plt
+
+arvore = tree.DecisionTreeClassifier(random_state=42, max_depth=5)
+arvore.fit(X_train, y_train)
+
+plt.figure(dpi=800, figsize=[4,4])
+# verificando quais variaveis podem ser importantes para a analiza
+tree.plot_tree(arvore, feature_names=X_train.columns,
+         filled=True,
+         class_names=[str(i) for i in arvore.classes_]
+         )
+
+# %% usando todos os ramos
+arvore = tree.DecisionTreeClassifier(random_state=42, max_depth=None)
+arvore.fit(X_train, y_train)
+feature_importances = (pd.Series(arvore.feature_importances_, index=X_train.columns)
+ .sort_values(ascending=False)
+ .reset_index())
+
+feature_importances['acum.'] = feature_importances[0].cumsum()
+feature_importances # um exemplo seria pegar até 95%
+feature_importances[feature_importances["acum."] < 0.96]
+
+# %%
+best_fetures = (feature_importances[feature_importances["acum."] < 0.96]['index']
+                .tolist())
+best_fetures
+
+# %%
+#============================ MODIFY ============================
+from feature_engine import discretisation, encoding
+from sklearn import pipeline
+
+# Discretizar
+tree_discretisation = discretisation.DecisionTreeDiscretiser(
+    variables=best_fetures,
+    regression=False,
+    cv=3,
+    bin_output="bin_number" # faz sair o numero do BIN, e isso é o nó da arvore que a observação caiu
+    )
+tree_discretisation.fit(X_train[best_fetures], y_train) # esta aprendendo 
+X_train_transform = tree_discretisation.transform(X_train[best_fetures])
+X_train_transform
+
+# Onehot
+onehot = encoding.OneHotEncoder(variables=best_fetures, ignore_format=True)
+onehot.fit(X_train_transform, y_train)
+
+# isso é feito, pois por serem valores numéricos, o modelo pode entender que são valores continuos (ou seja 3 é maior que 2), e não "niveis"
+X_train_transform = onehot.transform(X_train_transform)
+X_train_transform
+
+# %%
+# arvore_nova = tree.DecisionTreeClassifier(random_state=42)
+# arvore_nova.fit(X_train_transform, y_train)
+
+# (pd.Series(arvore_nova.feature_importances_, index=X_train_transform.columns)
+#    .sort_values(ascending=False)
+#    )
+
+# %%
+#============================ MODEL NAO APROFUNDADDO ============
+from sklearn import linear_model
+from sklearn import naive_bayes
+from sklearn import ensemble
+
+# model = linear_model.LogisticRegression(penalty=None, random_state=42, max_iter=100000)
+# reg.fit(X_train_transform, y_train)
+# model = naive_bayes.BernoulliNB()
+# model = ensemble.RandomForestClassifier(random_state=42,
+#                                         min_samples_leaf=20,
+#                                         n_jobs=-1, # é o numero de nucleos que serão usados para rodas as N arvores (-1 = max)
+#                                         n_estimators=500 # numero de arvores
+#                                         )
+model = ensemble.AdaBoostClassifier(random_state=42,
+                                    n_estimators=500,
+                                    learning_rate=0.01)
+
+
+# pipeline automatiza o processo de transformação
+model_pipeline = pipeline.Pipeline( 
+    steps=[
+        ("Discretizar", tree_discretisation),
+        ("Onehot", onehot),
+        ("Model", model),
+    ]
+)
+
+model_pipeline.fit(X_train, y_train)
+
+
+
+# %%
+from sklearn import metrics
+
+# y_train_predict = reg.predict(X_train_transform)
+# y_train_proba = reg.predict_proba(X_train_transform)[:,1] #curva roc
+y_train_predict = model_pipeline.predict(X_train)
+y_train_proba = model_pipeline.predict_proba(X_train)[:,1]
+
+
+acc_train = metrics.accuracy_score(y_train, y_train_predict)
+auc_train = metrics.roc_auc_score(y_train, y_train_proba)
+roc_train = metrics.roc_curve(y_train, y_train_proba)
+print("Acurácia Treino: ", acc_train)
+print("AUC Treino: ", auc_train)
+
+# %%    
+#============================ USANDO NO TESTE ===================
+# X_test_transform = tree_discretisation.transform(X_test[best_fetures])
+# X_test_transform = onehot.transform(X_test_transform)
+# y_test_predict = reg.predict(X_test_transform)
+# y_test_proba = reg.predict_proba(X_test_transform)[:,1] #curva roc
+y_test_predict = model_pipeline.predict(X_test)
+y_test_proba = model_pipeline.predict_proba(X_test)[:,1]
+
+
+acc_test = metrics.accuracy_score(y_test, y_test_predict)
+auc_test = metrics.roc_auc_score(y_test, y_test_proba)
+roc_test = metrics.roc_curve(y_test, y_test_proba)
+print("Acurácia Teste: ", acc_test)
+print("AUC Teste: ", auc_test)
+
+# %%    
+#============================ USANDO NO Out Of Time =============
+# oot_transform = tree_discretisation.transform(oot[best_fetures])
+# oot_transform = onehot.transform(oot_transform)
+# y_oot_predict = reg.predict(oot_transform)
+# y_oot_proba = reg.predict_proba(oot_transform)[:,1] #curva roc
+
+y_oot_predict = model_pipeline.predict(oot[features])
+y_oot_proba = model_pipeline.predict_proba(oot[features])[:,1]
+
+acc_oot = metrics.accuracy_score(oot[target], y_oot_predict)
+auc_oot = metrics.roc_auc_score(oot[target], y_oot_proba)
+roc_oot = metrics.roc_curve(oot[target], y_oot_proba)
+print("Acurácia OOT: ", acc_oot)
+print("AUC OOT: ", auc_oot)
+
+# %%
+plt.figure(dpi=400)
+plt.plot(roc_train[0], roc_train[1])
+plt.plot(roc_test[0], roc_test[1])
+plt.plot(roc_oot[0], roc_oot[1])
+plt.grid(True)
+plt.title("Curva ROC")
+plt.legend([
+    f"Treino: {100*auc_train:.2f}",
+    f"Teste: {100*auc_test:.2f}",
+    f"Out-of-Time: {100*auc_oot:.2f}"
+])
 # %%
--- 04_Machine_Learning/05_Projeto_Churn/README.md
+++ 04_Machine_Learning/05_Projeto_Churn/README.md
@@ -1,4 +1,7 @@
 # Projeto Churn
+**Churn**, em português "taxa de cancelamento" ou "taxa de rotatividade", é uma métrica que mede a percentagem de clientes que deixaram de utilizar um produto ou serviço numa empresa num determinado período. Este indicador é crucial, especialmente em modelos de negócio recorrentes, como assinaturas e contratos contínuos, pois afeta diretamente o faturamento e o crescimento da empresa. Uma taxa de churn baixa é um sinal positivo, indicando que a empresa tem uma boa capacidade de reter os seus clientes. 
+
+
 ## SEMMA
 O SEMMA é uma metodologia criada pela própria SAS Institute para orientar projetos de mineração de dados (Data Mining).
 
@@ -45,3 +48,87 @@ Exemplo: Se você tem 10.000 transações “não fraude” e 1.000 “fraude”
 Aumenta o tamanho da classe minoritária (a que tem menos exemplos), replicando ou gerando novos dados.
 
 Exemplo: Se você tem 10.000 “não fraude” e 1.000 “fraude”, pode duplicar/gerar novas amostras da classe “fraude” até chegar também em 10.000 → ficando 10.000 x 10.000.
+
+## Explore
+É o momento da metodologia em que fazemos uma analise de seleção do que será análizado. **Exemplo:** Uma análise descritiva, Análise Bivariana, *Indendificação de Missings/NaN*
+
+## Modify
+É fazer uma transformação na tabela. *No sklearn é a parte de [Processing](https://scikit-learn.org/stable/modules/preprocessing.html)*
+
+### Padronização Dos Dados
+É ideal que haja uma padronização na escala dos dados, antes deles serem usados nos modelos. Aqui usaremos a **Padronização pela Normal** ![](00_imagens\formula_padronizacao.png)
+
+### Imputação de Missing
+#### Ex: Falta Tempo Pra Voltar
+*"Vai faltar dado se essa pessoa nunca voltar"*
+
+Uma tratamento que pode ser feito em relação a isso, é pegar a maior data de volta que tivemos.
+
+#### Ex: Pontos Gastos Na Terça-feira
+*"Vai faltar dados se essa pessoa nunca comprou nada terça-feira*
+
+Um tratamento adequado para isso é trocar esse valor para 0
+
+#### Ex: Data de Nascimento/Idade
+Esse é um caso mais complicado, já que a idade pode estar interferindo, e não tem algo que possa ser feito de forma direta, como escolher o minimo, maximo, media, mediana, etc. Não é possível ter certeza, já que justamente a idade sair desses valores pode ser um motivo para esse resultado.
+
+Então uma coisa que pode ser feita para tratar esses dados seria criar um modelo de predição com as lihas que possuimos a idade, e tentar prever qual ida
+de provavelmente aquela linha teria.
+
+[Texto do Téo Calvo sobre isso](https://teobcalvo.wordpress.com/2017/12/16/machine-learning-para-imputacao-de-missings)
+
+### Binning
+É quando se possui uma váriavel **continua** e a transformamos em **faixas**
+#### Ex: Idades
+X = 13, 24, 32, 58, 64, ...
+
+Podemos separar por faixas simples:
+F(x) = [16-25], [26-35], [36-45], [46-55], [56-65], ...
+
+Podemos também separar essas faixas por um meio dinamico:
+Exemplo: usando uma arvore binaria com a variavel resposta, para assim que ela separe da "melhor maneira" os bins. Alguns são contra essa idéia por usarmos a variavel resposta para fazer uma "predição" porem, como isso é feito com uma base de treinameto, a utilização dela nas outras bases (Teste e OOT) em geral não cria um problema
+
+#### Variáveis de Classificação Ex: Escolaridade
+Vamos supor que exista essas escoláridades:
+|Sigla|Escolaridade|
+|-----|------------|
+|S|Superior|
+|SI|Superior Incompleto|
+|M|Medio|
+|MI|Medio Incompleto|
+|F|Fundamental|
+|FI|Fundamental Incompleto|
+
+E que possuimos uma tabela com uma coluna de escolaridade, o que fariamos para trartar isso seria usar o ***OneHot encoding*** Transformando Cada Nivel em uma váriavel separada:
+| Nome    | Escolaridade | E_F | E_M | E_MI | E_S |
+| ------- | ------------ | --- | --- | ---- | --- |
+| Ana     | S            | 0   | 0   | 0    | 1   |
+| Carlos  | M            | 0   | 1   | 0    | 0   |
+| Beatriz | M            | 0   | 1   | 0    | 0   |
+| João    | F            | 1   | 0   | 0    | 0   |
+| Maria   | S            | 0   | 0   | 0    | 1   |
+| Pedro   | MI           | 0   | 0   | 1    | 0   |
+| Lucas   | M            | 0   | 1   | 0    | 0   |
+| Júlia   | S            | 0   | 0   | 0    | 1   |
+
+Mas qual o problema disso? Pode acontecer de isso se tornar uma tabela gigante. Para solucionar isso temos outra possibilidade, que é usar o ***Mean encoder***. Que se basea em fazer uma média da váriavel resposta de Cada Nível, e substituir os Nìveis por essa Média:
+
+| Escolaridade | Média_Nota |
+| ------------ | ---------- |
+| F            | 4.0        |
+| M            | 6.0        |
+| MI           | 7.0        |
+| S            | 9.0        |
+
+| Nome    | Escolaridade |
+| ------- | ------------ |
+| Ana     | 0,90         |
+| Carlos  | 0,60         |
+| Beatriz | 0,60         |
+| João    | 0,40         |
+| Maria   | 0,90         |
+| Pedro   | 0,70         |
+| Lucas   | 0,60         |
+| Júlia   | 0,90         |
+
+Por ultimo podemos também usar o ***OneHot encoding*** porem, agrupar os níveis. Como exemplo: "Em vez de separa cada estado, Vamos agrupa-los em regiões: sul, sudeste, etc."
